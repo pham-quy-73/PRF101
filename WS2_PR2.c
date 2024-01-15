9d7fc5b75f3f53ce258efa64050cd1dd71a65c73
pham-quy-73 <zunazardy@gmail.com>
2024-01-15 21:22:57 +0700

    WR2

---
+++ WS2_PR2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	long pa = 9000000, pd = 3600000;
+	long tf, n, ti, m;
+	printf("Your income this year: ");
+	scanf("%ld", &m);
+	printf("Number of dependents: ");
+	scanf("%ld", &n);
+	tf = 12 * (pa + n * pd);
+	printf("Tax-free income: %ld\n", tf);
+	ti = m - tf;
+	if (ti <= 0)
+	{
+		printf("Taxable income: 0\n");
+		printf("Income tax: 0\n");
+	}
+	else if (ti > 0 && ti <= 5000000)
+	{
+    		printf("Taxable income: %ld\n", ti);
+    		printf("Income tax: %ld\n", ti*5/100);
+	}
+	else if (ti >= 5000001 && ti <= 10000000)
+	{
+    		printf("Taxable income: %ld\n", ti);
+    		printf("Income tax: %ld\n", ti*10/100);
+	}
+    	else if (ti >= 10000001 && ti <= 18000000)
+	{
+    		printf("Taxable income: %ld\n", ti);
+    		printf("Income tax: %ld\n", ti*15/100);
+	}
+    	else
+	{
+		printf("Taxable income: %ld\n", ti);
+    		printf("Income tax: %ld\n", ti*20/100);
+	}	
+	return 0;
+}
